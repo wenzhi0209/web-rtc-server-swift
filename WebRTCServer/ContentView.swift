79e796575192298c5f842784a26d94efe48bf3e8
v_fwzteo <v_fwzteo@tencent.com>
2025-12-05 18:35:49 +0800

    Initial Commit

---
+++ WebRTCServer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WebRTCServer
+//
+//  Created by TEO WEN ZHI on 05/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WebRTCServer/WebRTCServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebRTCServerApp.swift
+//  WebRTCServer
+//
+//  Created by TEO WEN ZHI on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct WebRTCServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
